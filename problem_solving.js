a6677e1df308d3405f039282c568710c0519c006
developer-shadhin <sabbirtech.com@gmail.com>
2025-10-15 14:17:35 +0600

    problem solving codes

---
+++ problem_solving.js
@@ -0,0 +1,126 @@
+//problem 01- Check Even or Odd
+let num = 7;
+let result = num % 2 === 0 ? "Even" : "Odd";
+
+console.log(result);
+
+
+// //Check Positive, Negative or Zero
+// let n = -5;
+
+// if (n > 0){
+//     console.log("Positive");
+// }else if (n < 0){
+//     console.log("Negative");
+// }
+// else console.log("Zero");
+
+
+// //Find Maximum of Two Numbers
+// let a = 10; 
+// b = 20;
+// let max = a > b ? a : b;
+
+// console.log(max);
+
+// //Find Maximum of Three Numbers
+// let x = 5;
+// y = 20;
+// z = 10;
+
+// if (x > y && x > z){
+//     console.log(x);
+// }else if (y > x && y > z){
+//     console.log(y);
+// }
+// else console.log(z);
+
+// //Check Leap Year
+// let year = 2004;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
+//     console.log("Leap Year");
+// }
+// else{
+//     console.log("Not Leap Year");
+// }
+
+// //Check Voting Eligibility
+// let age = 20;
+
+// let voting = age >= 18 ? "You are eligible" : "you are not eligible";
+// console.log(voting);
+
+// //Check Divisibility by 5 and 11
+// let number = 55;
+
+// if (number % 5 === 0 && number % 11 === 0){
+//     console.log("The number is Divisible");
+// }
+// else{
+//     console.log("The number is not Divisible");
+// }
+
+// //Grade Calculator
+// let marks = 85;
+
+// if (marks >= 80){
+//     console.log("Grade A");
+// }else if (marks >= 60){
+//     console.log("Grade B");
+// }else if (marks >= 40){
+//     console.log("Grade C");
+// } 
+// else console.log("Grade F");
+
+// //Check Uppercase or Lowercase
+// let ch = 'A';
+
+// let check = ch >= 'A'  ? "Uppercase" : ch >= 'a' ? "Lowercase" : "Invalid";
+// console.log(check);
+
+// // Check Vowel or Consonant`
+// let letter = 'a';
+// letter = letter.toLowerCase();
+
+// if (['a', 'e', 'i', 'o', 'u'].includes(letter)){
+//     console.log("The letter is Vowel");
+// }
+// else{
+//     console.log("The letter is Consonant");
+// }
+
+// //Check Multiple of 3 or 7
+// let number3 = 9;
+
+// let multi = number3 % 3 === 0 ? "Multiple of 3" : number3 % 7 === 0 ? "Multiple of 7" : "Not a multiple of 3 and 7";
+
+// console.log(multi);
+
+// //Absolute Value
+// let number8 = -8;
+
+// let value = number8 < 0 ? -number8 : number8;
+
+// console.log(value);
+
+// //Check Profit or Loss
+// let CP = 100;
+// let SP = 150;
+
+// if (SP > CP){
+//     console.log("Profit");
+// }else if (SP < CP){
+//     console.log("Loss");
+// }
+// else{
+//     console.log("No Profit No Loss");
+// } 
+
+
+
+
+
+
+
+
